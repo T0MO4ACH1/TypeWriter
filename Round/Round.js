036d5181a93dfd2ebada8a2726d148e4da29f264
Kushal Radadiya <kushalpatel8989@gmail.com>
2025-11-07 10:45:52 +0530

    Add files via upload

---
+++ Round/Round.js
@@ -0,0 +1,19 @@
+const options = document.querySelectorAll('.option');
+    const loading = document.getElementById('loading');
+
+    options.forEach(option => {
+      option.addEventListener('click', () => {
+        options.forEach(o => o.classList.remove('selected'));
+        option.classList.add('selected');
+
+        const file = option.getAttribute('data-file');
+        localStorage.setItem('selectedRound', file);
+
+        // loading overlay
+        loading.classList.add('active');
+
+        setTimeout(() => {
+          window.location.href = '../Gameplay/game.html';
+        }, 2000); // loading delay
+      });
+    });
